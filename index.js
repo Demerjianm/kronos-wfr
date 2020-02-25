d8f7afb0321ad6eaa6c46ac941830be52853ef3f
Michael Demerjian <michaeldemerjian@gmail.com>
2020-02-24 23:05:52 -0800

    first comit

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-const express = require("express");
-const { getEmployees, getEmployees2 } = require("./kronos");
+import express from 'express';
+import Kronos from './kronos';
+
 const app = express();
 const port = 3001;
+// const api = new Kronos({
+//   username: process.env.WFRUSERNAME,
+//   password: process.env.WFRPASSWORD,
+//   apikey: process.env.APIKEY,
+//   company: process.env.WFRCOMPANY
+// });
 
-isAsync = async () => {
-  getEmployees();
-};
-// getEmployees();
-// setTimeout(() => {
-//   getEmployees2();
-// }, 5000);
-// isAsync();
 app.listen(port, async () => {
-  await getEmployees();
   console.log(`woo running, ${port}`);
 });
--- kronos.js
+++ kronos.js
@@ -1,75 +1,88 @@
-const axios = require("axios");
-require("dotenv").config();
-const staticHeaders = {
-  Accept: "application/json",
-  "Content-Type": "application/json"
-};
-const company = process.env.WFRCOMPANY;
-const key = process.env.APIKEY;
-const config = {
-  credentials: {
-    username: process.env.WFRUSERNAME,
-    password: process.env.WFRPASSWORD,
-    company
+const axios = require('axios');
+require('dotenv').config();
+
+export default class Kronos {
+  constructor({ username, password, apikey, company }) {
+    this.credentials = { username, password, company };
+    this.apikey = apikey;
+    this.activeToken = null;
+    this.staticHeaders = {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    };
   }
-};
 
-let activeToken;
+  /**
+   * @description - creates the headers for api requests
+   */
+  async handleHeaders() {
+    const token = await this.auth();
+    return {
+      headers: {
+        Authentication: `bearer ${token}`,
+        ...this.staticHeaders
+      }
+    };
+  }
 
-auth = async () => {
-  var options = {
-    method: "POST",
-    url: "https://secure2.saashr.com/ta/rest/v1/login?origin=script",
-    headers: {
-      ...staticHeaders,
-      "api-key": key
-    },
-    data: JSON.stringify({ ...config })
-  };
-  try {
-    if (activeToken) {
-      return activeToken;
-    } else {
+  /**
+   *
+   * @param {object} options - the options axios needs to make a requests
+   *  - method, url, headers, data
+   */
+  async handleAxios({ options }) {
+    try {
+      const res = await axios(options);
+      const { data } = res;
+      return data;
+    } catch (e) {
+      console.error(e);
+      return e;
+    }
+  }
+
+  /**
+   * @description handles getting the auth token for making following requests
+   *  if a token is already in the model just returns that instead of requery
+   */
+  async auth() {
+    const options = {
+      method: 'POST',
+      url: 'https://secure2.saashr.com/ta/rest/v1/login?origin=script',
+      headers: {
+        ...this.staticHeaders,
+        'api-key': this.apikey
+      },
+      data: JSON.stringify({
+        credentials: { ...this.credentials }
+      })
+    };
+    // if token exist just return
+    if (this.activeToken) {
+      return this.activeToken;
+    }
+    try {
       const res = await axios(options);
       const {
         data: { token }
       } = res;
-      activeToken = token;
+      this.activeToken = token;
       return token;
+    } catch (e) {
+      console.error(e);
+      return e;
     }
-  } catch (e) {
-    console.log(e);
   }
-};
 
-handleHeaders = async () => {
-  const tk = await auth();
-  return {
-    headers: {
-      Authentication: `bearer ${tk}`,
-      ...staticHeaders
-    }
-  };
-};
-
-getEmployees = async () => {
-  var options = {
-    method: "GET",
-    url: `https://secure2.saashr.com/ta/rest/v2/companies/|${company}/employees`,
-    ...(await handleHeaders())
-  };
-  return handleAxios({ options });
-};
-
-handleAxios = async ({ options }) => {
-  try {
-    const res = await axios(options);
-    const { data } = res;
-    return data;
-  } catch (e) {
-    console.log(e);
-    return e;
+  /**
+   * @description fetches employees from a specific company
+   */
+  async getEmployees() {
+    const options = {
+      method: 'GET',
+      url: `https://secure2.saashr.com/ta/rest/v2/companies/|${this.credentials.company}/employees`,
+      ...(await this.handleHeaders())
+    };
+    return this.handleAxios({ options });
   }
-};
-
-module.exports = { getEmployees };
+}
